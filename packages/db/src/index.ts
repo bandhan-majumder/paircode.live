aa5864e208775ddf16886800677fb4805e582de3
Bandhan Majumder <bandhanmajumder16@gmail.com>
2025-12-13 18:58:30 +0530

    enable query for neon

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -11,7 +11,8 @@ import ws from "ws";
 neonConfig.webSocketConstructor = ws;
 
 // To work in edge environments (Cloudflare Workers, Vercel Edge, etc.), enable querying over fetch
-// neonConfig.poolQueryViaFetch = true
+
+neonConfig.poolQueryViaFetch = true
 
 const sql = neon(process.env.DATABASE_URL || "");
 export const db = drizzle(sql);
