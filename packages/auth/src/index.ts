6e829f3a183908bee85b0d1c711613b2e5c23545
Bandhan Majumder <bandhanmajumder16@gmail.com>
2025-12-20 13:35:43 +0530

    add: gh actions and cookie settings update

--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -8,7 +8,6 @@ import { db } from "@paircode/db";
 import * as schema from "@paircode/db/schema/auth";
 
 export const auth = betterAuth<BetterAuthOptions>({
-	baseURL: "https://backend.paircode.live",
 	database: drizzleAdapter(db, {
 		provider: "pg",
 
@@ -24,12 +23,13 @@ export const auth = betterAuth<BetterAuthOptions>({
 	advanced: {
 		crossSubDomainCookies: {
 			enabled: true,
-			domain: '.paircode.live',
+			domain: 'backend.paircode.live',
 		},
 		useSecureCookies: true,
 		defaultCookieAttributes: {
 			sameSite: "none", 
 			secure: true,
+			httpOnly: true,
 		}
 	},
 	// BETTER_AUTH_URL is set as baseURL
@@ -37,15 +37,5 @@ export const auth = betterAuth<BetterAuthOptions>({
 		"https://paircode.live",
 		"https://backend.paircode.live",
 		...(process.env.NODE_ENV === 'development' ? ["http://localhost:3000", "http://localhost:3001"] : [])
-	],
-	session: {
-        expiresIn: 60 * 60 * 24 * 7, // 7 days
-        updateAge: 60 * 60 * 24, // 1 day (every 1 day the session expiration is updated)
-		freshAge: 60 * 5, // 5 minutes
-		cookieCache: {
-            enabled: true,
-            maxAge: 5 * 60, // Cache duration in seconds (5 minutes)
-			strategy: "compact"
-        }
-    }
+	]
 });
