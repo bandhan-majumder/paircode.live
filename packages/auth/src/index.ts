1e07a97a0552902bff2cdea4aedae81b78c0eff4
Bandhan Majumder <bandhanmajumder16@gmail.com>
2025-12-22 12:14:33 +0530

    update

--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -1,4 +1,5 @@
 import { betterAuth, type BetterAuthOptions } from "better-auth";
+import { nextCookies } from "better-auth/next-js";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "@paircode/db";
 import * as schema from "@paircode/db/schema/auth";
@@ -6,19 +7,22 @@ import * as schema from "@paircode/db/schema/auth";
 export const auth = betterAuth<BetterAuthOptions>({
 	database: drizzleAdapter(db, {
 		provider: "pg",
-
 		schema: schema,
 	}),
-	rateLimit: {
-		enabled: true,
-		window: 10, // time window in seconds
-		max: 100, // max requests in the window
-	},
 	socialProviders: {
 		google: {
-			prompt: "select_account",
+			prompt: "select_account", 
 			clientId: process.env.GOOGLE_CLIENT_ID || "",
 			clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
 		},
 	},
+	// advanced: {
+	// 	defaultCookieAttributes: {
+	// 		sameSite: "none",
+	// 		secure: true,
+	// 		httpOnly: true,
+	// 	},
+	// },
+	trustedOrigins: ["https://paircode.live"],
+	plugins: [nextCookies()],
 });
