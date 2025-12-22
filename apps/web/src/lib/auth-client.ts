ada0ea53965daa8554aa6750105d2f1e70e3ad57
Bandhan Majumder <bandhanmajumder16@gmail.com>
2025-12-22 19:04:07 +0530

    add: pre commit hook & fix cookies

--- apps/web/src/lib/auth-client.ts
+++ apps/web/src/lib/auth-client.ts
@@ -1,14 +1,6 @@
-import type { auth } from "@paircode/auth";
 import { createAuthClient } from "better-auth/react";
-import { inferAdditionalFields } from "better-auth/client/plugins";
 
-export const authClient = createAuthClient({
-	baseURL: "https://paircode.live",
-	plugins: [inferAdditionalFields<typeof auth>()],
-	fetchOptions: {
-		credentials: "include",
-	},
-});
+export const authClient = createAuthClient();
 
 export const signInWithGoogle = async () => {
 	await authClient.signIn.social({
--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -1,39 +1,25 @@
 import { betterAuth, type BetterAuthOptions } from "better-auth";
-import { nextCookies } from "better-auth/next-js";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "@paircode/db";
 import * as schema from "@paircode/db/schema/auth";
 
 export const auth = betterAuth<BetterAuthOptions>({
-	baseURL: "https://paircode.live",
 	database: drizzleAdapter(db, {
 		provider: "pg",
 		schema: schema,
 	}),
-	socialProviders: {
-		google: {
-			prompt: "select_account", 
-			clientId: process.env.GOOGLE_CLIENT_ID || "",
-			clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
-		},
-	},
-	advanced: {
-		defaultCookieAttributes: {
-			sameSite: "lax",
-			secure: true,
-			httpOnly: true,
-			domain: ".paircode.live",
-		},
-	},
-	session: {
-		cookieCache: {
-			enabled: true,
-			maxAge: 5 * 60, // 5 minutes
-		},
-	},
-	trustedOrigins: [
-		"https://paircode.live",
-		"https://backend.paircode.live",
-	],
-	plugins: [nextCookies()],
+	rateLimit: {
+    enabled: true,
+    window: 10, // time window in seconds
+    max: 100, // max requests in the window
+  },
+  socialProviders: {
+    google: {
+      accessType: "offline",
+      prompt: "select_account consent",
+      clientId: process.env.GOOGLE_CLIENT_ID || "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+    },
+  },
+  trustedOrigins: [process.env.CORS_ORIGIN || "http://localhost:3001"],
 });
