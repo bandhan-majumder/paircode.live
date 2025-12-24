d1c75d4c9ca15e30e04c805aec0be67b47aac515
Bandhan Majumder <bandhanmajumder16@gmail.com>
2025-12-24 21:24:41 +0530

    delete svgs and extra fonts

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type {Metadata} from "next";
-import { Geist, Geist_Mono, Raleway, Shadows_Into_Light } from "next/font/google";
+import { Geist, Geist_Mono } from "next/font/google";
 import "../index.css";
 import Providers from "@/components/providers";
 import localFont from "next/font/local";
@@ -8,17 +8,6 @@ export const sekuyaFont = localFont({
 	src: "../../public/font/Sekuya-Regular.ttf"
 });
 
-export const shadowsIntoLight = Shadows_Into_Light({
-  subsets: ["latin"],
-  variable: "--font-shadows-into-light",
-  weight: "400",  
-  display: "swap",
-});
-
-export const railwayFont = Raleway({
-	variable: "--font-geist-sans",
-	subsets: ["latin"],
-})
 
 const geistSans = Geist({
 	variable: "--font-geist-sans",
