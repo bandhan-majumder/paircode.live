91697c6f717ae1d21f17f8f1f00db5064e6a7d72
Bandhan Majumder <bandhanmajumder16@gmail.com>
2025-12-24 12:53:28 +0530

    svg update

--- apps/web/src/components/hero-section.tsx
+++ apps/web/src/components/hero-section.tsx
@@ -28,7 +28,7 @@ export default function HeroSection() {
       </div>
 
       <div className="flex justify-center items-center md:hidden mb-8">
-        <Image src={"/paircode-ascii.svg"} width={230} height={200} alt="PairCode" className="" />
+        <Image src={"/paircode-ascii.svg"} width={230} height={200} alt="PairCode" className="z-10" />
       </div>
 
       <div className="text-center">
